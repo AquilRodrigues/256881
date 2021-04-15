658924f475e6aa00f141c442d925d6f04eaacad9
Aquil Rodrigues <aquil.ee17@bmsce.ac.in>
2021-04-15 21:19:32 +0530

    few changes made

--- 3_Implementation/src/induction_input.c
+++ 3_Implementation/src/induction_input.c
@@ -4,25 +4,25 @@ void induction_input(){
         int pole;  
         float voltage;
         float power;
-        int slip;
+        float slip;
         float freq;
         float pf;
         
     }ac;
     ac m;
     
-    float n,t,i,ratedtq,speed;
-    float *ptr;
+    float t,ratedtq,speed;
+    int n;
     printf("entre number of poles");
     scanf("%d",&m.pole);          
     printf("entre rated volatage");
-    scanf("%d",&m.voltage);
+    scanf("%f",&m.voltage);
     printf("entre rated power of machine");
-    scanf("%d",&m.power);
+    scanf("%f",&m.power);
     printf("entre rated speed");
-    scanf("%d",&m.slip);
+    scanf("%f",&m.slip);
     printf("entre the line frequency");
-    scanf("%d",&m.freq);
+    scanf("%f",&m.freq);
     printf("entre number of division of torque to calculate current for each");
     scanf("%d",&n);
 
